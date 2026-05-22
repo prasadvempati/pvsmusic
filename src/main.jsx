1629f288c3fd4992321bad345bbff959f5b34329
prasadvempati <36576047+prasadvempati@users.noreply.github.com>
2026-05-22 01:18:54 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import Privacy from "./Privacy.jsx";
+
+const isPrivacy = window.location.pathname === "/privacy";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {isPrivacy ? <Privacy /> : <App />}
   </StrictMode>
 );
